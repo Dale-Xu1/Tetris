c611aaba25042175baf61010aec132c5e3ced054
Dale Xu <69087617+Dale-Xu1@users.noreply.github.com>
2020-08-25 10:55:50 -0400

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    public class GameManager : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            Debug.Log("Hello world");
+        }
+
+    }
+}
